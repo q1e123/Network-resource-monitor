0ebc3d3f787c22923029ea42cafdcabe395b99df
Robert <robertnicolaesolca@protonmail.com>
2020-08-21 15:49:19 +0300

    db changes

--- SQL/create-usage_data-table.sql
+++ SQL/create-usage_data-table.sql
@@ -5,6 +5,7 @@ CREATE TABLE Usage_Data(
     free_ram int,
     timestamp datetime,
     current_user varchar(256),
+    operating_system varchar(256)
 
     system_id int,
     CONSTRAINT FK_Systems_Usage_Data FOREIGN KEY (system_id) REFERENCES Systems (id)
