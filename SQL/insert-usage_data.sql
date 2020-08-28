63e09563e6febd6ad8d539589a07f9bfeb47215c
Robert <robertnicolaesolca@protonmail.com>
2020-08-28 14:14:51 +0300

    updated sql for avalabile space

--- SQL/insert-usage_data.sql
+++ SQL/insert-usage_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO Usage_Data(total_ram, used_ram, free_ram, usage_timestamp, username, operating_system, system_id)
-VALUES (:total_ram, :used_ram, :free_ram, :usage_timestamp, :user, :operating_system, :system_id);
+INSERT INTO Usage_Data(total_ram, used_ram, free_ram, avalabile_space, usage_timestamp, username, operating_system, system_id)
+VALUES (:total_ram, :used_ram, :free_ram, :avalabile_space, :usage_timestamp, :user, :operating_system, :system_id);
